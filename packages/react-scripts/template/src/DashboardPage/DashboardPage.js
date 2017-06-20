275b34b9861719072e4eee3f5bfcdc84d12cc89d
Damian Sopel <dsopel0@gmail.com>
2017-06-20 14:31:15 -0500

    Buttons are rendering from mlab

--- packages/react-scripts/template/src/DashboardPage/DashboardPage.js
+++ packages/react-scripts/template/src/DashboardPage/DashboardPage.js
@@ -18,32 +18,37 @@ class DashboardPage extends React.Component {
   }
 
   componentDidMount() {
-    axios.get('localhost:3001/courses').then(response => {
-      console.log(
-        'I am getting the response from axios courses here!',
-        response.data
+    axios.get('http://localhost:3001/courses').then(response => {
+      const courses = response.data.courses;
+      const userIds = Object.keys(courses).map(
+        course => courses[course]._creator
       );
-      console.log(this);
       this.setState({
-        courses: response.data,
+        courses: response.data.courses,
       });
     });
   }
   render() {
-    let inst = cookies.get('instructor');
+    let inst = cookies.get('instructor').fullName;
     console.log('ins', inst);
-    inst = inst.fullName;
-    //CourseController.getAllCourses((req,res) => {
-    //console.log(res.data)
-    //})
+
+    let courseList = this.state.courses;
+    let courses = Object.keys(courseList).map(course => courseList[course]);
+    const courseButtons = courses.map(course => {
+      return (
+        <p>
+          <button className="courses" id={course._id}>{course.name}</button>
+        </p>
+      );
+    });
     return (
       <div>
-        <div className="greeting"> Welcome Back, {this.props.fullName}</div>
+        <div className="greeting"> Welcome Back, {inst}</div>
         <div className="addCourseLink">
           <Link to="/addCourse">Add a new course </Link>
         </div>
         <div className="dashboard-your-courses"><h2>Your Courses</h2></div>
-        <div className="courseList">{this.state.courses.name}</div>
+        <div className="courseList">{courseButtons}</div>
       </div>
     );
   }
